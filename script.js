3a56c24ca47afd6334b424f0e697193813f85f71
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-05-02 21:43:27 +0530

    js added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// let buttons = document.querySelectorAll('.buttons');
+let inputButton = document.querySelectorAll(".buttons input");
+let display = document.getElementById("screen");
 
+for (let i = 0; i < inputButton.length; i++) {
+  inputButton[i].addEventListener("click", function () {
+    // display.value = inputButton[i];
+    // display.value = this.value;
+
+   
+  });
+}
